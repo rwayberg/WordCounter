7c5688b049b7ec670d6fa4c02889cddfb50e2784
Randy Wayberg <rwayberg@gmail.com>
2025-03-24 13:50:38 -0400

    Remove TextBox text from displays. Add gitignore file

--- obj/Debug/net9.0-windows/WordCounter.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/WordCounter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordCounter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a248cc9cd5447f41ec76824e1bf57edccf9ac8a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordCounter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordCounter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
